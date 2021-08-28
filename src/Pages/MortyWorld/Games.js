fbddb7372115952d2416adfcb519279ce0a141c2
dcartist <pbannerman13@gmail.com>
2021-08-28 19:45:02 -0400

    adding more morties animation

--- src/Pages/MortyWorld/Games.js
+++ src/Pages/MortyWorld/Games.js
@@ -1,34 +1,42 @@
 import React, {useState} from 'react'
 import Morty from "../../Images/morty.svg"
-import { Stage, Sprite } from '@inlet/react-pixi';
 export default function Games() {
-    const [morty, setmorty] = useState({class:{height:"200px"}, score:20})
+    const image = [
+        "/images/morty.svg", 
+        "/images/morty1.svg"
+    ]
+    const [morty, setmorty] = useState({class:{height:"200px"}, score:20, imgurl: image[0]})
     const [morties, setmorties] = useState([morty])
     const [score, setscore] = useState(0)
     const [size, setsize] = useState(100)
     const [count, setcount] = useState(0)
+    
 
     function multiMorty(info){
-        console.log(count)
+
         if (count <= 3){
+            let newState = [...morties];
+            newState[info].imgurl = image[1];
+            setmorty(newState)
             setscore(morties[info].score + score)
             setcount(count + 1)
             let newSize = size - 20
             setsize(newSize)
-            setmorties(morties.concat({class:{height:`${size}px`}, score:10}))
+            setmorties(morties.concat({class:{height:`${size}px`}, score:10, imgurl: image[0]}))
         } else {
             setscore(morties[info].score + score)
         }
         
 
     }
+    // console.log(imgurl)
     return (
         <div className="Morty">
             Score : {score}
            
   {morties.map((item, index) =>
    <div class="path">
-   <img src={Morty} style={item.class}className="shape" onClick={()=>multiMorty(index)}></img>
+   <img src={process.env.PUBLIC_URL + item.imgurl} style={item.class}className="shape" onClick={()=>multiMorty(index)}></img>
  </div>)}
      
             
