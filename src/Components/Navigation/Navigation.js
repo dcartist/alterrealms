802b9000a55b2bed8ab8ee1d005f32ef4face427
dcartist <pbannerman13@gmail.com>
2021-08-29 10:59:27 -0400

    adding menu bar

--- src/Components/Navigation/Navigation.js
+++ src/Components/Navigation/Navigation.js
@@ -4,23 +4,45 @@ import {Link, useLocation} from 'react-router-dom'
 
 export default function Navigation() {
     const location = useLocation();
+    /* Set the width of the sidebar to 250px (show it) */
+function openNav() {
+    document.getElementById("sidepanel").style.width = "350px";
+  }
+  
+  /* Set the width of the sidebar to 0 (hide it) */
+  function closeNav() {
+    document.getElementById("sidepanel").style.width = "0";
+  }
     if (location.pathname == "/"){
         return(
-            <div></div>
+           <div>
+               <button className="openbtn" onClick={openNav}>&#9776; Alter Menu</button>
+               <div id="sidepanel" className="sidepanel">
+  <a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+ 
+                        <Link to="/home" onClick={closeNav}>Home</Link>
+                        <Link to="/about" onClick={closeNav}>About</Link>
+                        <Link to="/sm" onClick={closeNav}>Speengle-Meengle</Link>
+                        <Link to="/morty" onClick={closeNav}>Morty World</Link>
+                
+</div>
+           </div>
+
         )
     } else {
         return (
-            <div className="Navigation">
-                    <ul>
-                        <li><Link to="/home">home</Link></li>
-                        <li><Link to="/about">About</Link></li>
-                        <li><Link to="/sm">Speengle-Meengle</Link></li>
-                        <li><Link to="/morty">Morty World</Link></li>
-                    </ul>
-                    
-                    
-                    
-                </div>
+            <div>
+            <button className="openbtn" onClick={openNav}>&#9776; Galatic Menu</button>
+            <div id="sidepanel" className="sidepanel">
+<a href="javascript:void(0)" className="closebtn" onClick={closeNav}>&times;</a>
+
+                     <Link to="/home" onClick={closeNav}>home</Link>
+                     <Link to="/about" onClick={closeNav}>About</Link>
+                     <Link to="/sm" onClick={closeNav}>Speengle-Meengle</Link>
+                     <Link to="/morty" onClick={closeNav}>Morty World</Link>
+             
+</div>
+        </div>
         )
     }
  
