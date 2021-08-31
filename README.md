f48c9455123944de49a1dea74c4da613ca49b814
dcartist <pbannerman13@gmail.com>
2021-08-31 13:33:09 -0400

    updating README

--- README.md
+++ README.md
@@ -2,3 +2,90 @@
 
 Speengle-Meengle
 
+
+
+# A Simple War Game
+
+![alter realm](src/Images/alterRealm.png)
+
+## About
+
+**deployed link:** https://alterrealms.vercel.app
+
+**backend repo:** https://github.com/dcartist/alterrealms-api
+
+**backend link:** https://immense-refuge-56824.herokuapp.com/
+
+### **Created by:** 
+
+Paula Bannerman
+
+**Portfolio:** https://www.dcartist.studio
+
+**LinkedIn:** https://www.linkedin.com/in/dcartist
+
+**When:** built during the Mintbean hackathon
+**Duration of Creation:** 1 week
+
+## Installation Instructions
+
+- Clone and download the project
+- cd into the project 
+- use `npm install` to install the react app
+- use `npm run start` to start the project
+
+## Game Play Instructions:
+
+### Speengle Meengle:
+
+Speengle Meengle is a rick and morty version of rock paper scissors.
+
+Select a character through more than 600+ characters from the Rick and Morty show. Once you select a character, choose a weapon. Then continue to see who is the winner of the round.
+
+**To Win:** 
+
+**SpaceShip** Beats **Portal Ray**. 
+
+**Portal Ray** Beats **Robot**. 
+
+**Robot** Beats **Spaceship**
+
+### Game Play
+
+- Enter the game
+
+- Choose the character you want to user.
+
+- Select a Weapon
+
+- Press the `Fight!` button to view who won the match.
+
+  
+
+### Morty World:
+
+**To Win:**  Click on the morty head
+
+### Game Play
+
+- Enter the game
+
+- Click on the morty head as many times as you can within a 10 seconds.
+
+  
+
+## **Technologies Used:**
+
+- React JS Framework (hooks and classes)
+- Javascript
+- Axios
+- Sass
+- Express
+
+## Reasoning:
+
+As a boot camp graduate from General Assembly DC Chapter (Sep. 2019) and a mentor to other boot camp students. I desired to build this game out for a while now. Also, I enjoy building apps. 
+
+## Future Plans:
+
+To have autocompletion on searching characters and moving images so they load correctly.
--- src/Pages/About.js
+++ src/Pages/About.js
@@ -22,8 +22,8 @@ export default class About extends Component {
                <span>Portfolio: </span> <br></br><a href="https://www.dcartist.studio" target="_blank" rel="noreferrer" >www.dcartist.studio</a>
                </li>
                <li><span>LinkedIn: </span> <br></br><a href="https://www.linkedin.com/in/dcartist" rel="noreferrer" target="_blank">www.linkedin.com/in/dcartist</a></li>
-               <li><span>Github: </span> <br></br><a href="https://github.com/dcartist" rel="noreferrer" target="_blank">www.github.com/dcartist</a></li>
-               <li><span>Github Repo: </span> <br></br><a href="https://github.com/dcartist/Simple-War-Game" rel="noreferrer" target="_blank">www.github.com/dcartist/Simple-War-Game</a></li>
+               <li className="github"><span>Github: </span> <br></br><a href="https://github.com/dcartist" rel="noreferrer" target="_blank">www.github.com/dcartist</a></li>
+               <li><span>Github Repo: </span><br></br><br></br> Frontend: <br></br><a href="https://github.com/dcartist/alterrealms-api" rel="noreferrer" target="_blank">www.github.com/dcartist/alterrealms-api</a><br></br><br></br>Backend:<br></br> <a href="https://github.com/dcartist/alterrealms-api" rel="noreferrer" target="_blank">www.github.com/dcartist/alterrealms-api</a></li>
            </ul></div>
                     </div>
           
@@ -37,16 +37,18 @@ export default class About extends Component {
            <div className="about-technology">
            <h2><span>Technologies Used:</span></h2>
            <ul>
-               <li>ReactJS Framework (hooks and classes)</li>
+               <li>ReactJS Framework<br></br>(hooks and classes)</li>
                <li>Javascript</li>
+               <li>Axios</li>
                <li>Express</li>
                <li>Mongo DB</li>
                <li>Rick and Morty API</li>
            </ul>
            </div>
-            <div className="about-technology"> <h2><span>Reasoning:</span></h2><p>As a boot camp graduate from General Assembly DC Chapter (Sep. 2019) and a mentor to other boot camp students, I wanted to support them during this hackathon by creating my game. Also, I enjoy building apps. </p></div>
-            <div className="about-technology"> <h2><span>Future Plan:</span></h2><p>To have autocompletion on searching characters and moving images so they load correctly. </p>
-</div>
+            <div className="about-technology"> <h2><span>Reasoning:</span></h2><p>As a boot camp graduate from General Assembly DC Chapter (Sep. 2019) and a mentor to other boot camp students. I desired to build this game out for a while now. Also, I enjoy building apps.  </p>
+            <h2><span>Future Plan:</span></h2><p>To have autocompletion on searching characters and moving images so they load correctly. </p>
+            </div>
+            
            </div>
            
            
