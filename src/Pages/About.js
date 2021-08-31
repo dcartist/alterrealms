0174a179b0592017cb9f0f1c4cf466d987e55eb4
dcartist <pbannerman13@gmail.com>
2021-08-31 13:00:00 -0400

    updating about section

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -8,24 +8,32 @@ export default class About extends Component {
                 <div className="about-info">
                     <div className="">
                         <h1>Alter Realm</h1>
-                    <h3><span>Created by: </span>Paula Bannerman</h3>
-           <img src={Image} className="shadow"></img>
-                    </div>
-                
-                <ul>
+                        <ul>
                     <li><span>When: </span>built during the Mintbean hackathon</li>
                     <li><span>Duration of Creation: </span>1 week</li>
                 </ul>
-          
-
-           <ul>
+                    
+                    <div className="about-section">
+                    <img src={Image} className="shadow"></img>
+                    <div>
+                    <h3><span>Created by: </span>Paula Bannerman</h3>
+                        <ul>
                <li>
                <span>Portfolio: </span> <br></br><a href="https://www.dcartist.studio" target="_blank" rel="noreferrer" >www.dcartist.studio</a>
                </li>
                <li><span>LinkedIn: </span> <br></br><a href="https://www.linkedin.com/in/dcartist" rel="noreferrer" target="_blank">www.linkedin.com/in/dcartist</a></li>
                <li><span>Github: </span> <br></br><a href="https://github.com/dcartist" rel="noreferrer" target="_blank">www.github.com/dcartist</a></li>
                <li><span>Github Repo: </span> <br></br><a href="https://github.com/dcartist/Simple-War-Game" rel="noreferrer" target="_blank">www.github.com/dcartist/Simple-War-Game</a></li>
-           </ul>
+           </ul></div>
+                    </div>
+          
+                    </div>
+                
+                
+          
+
+           
+           <div className="about-infotech">
            <div className="about-technology">
            <h2><span>Technologies Used:</span></h2>
            <ul>
@@ -36,11 +44,15 @@ export default class About extends Component {
                <li>Rick and Morty API</li>
            </ul>
            </div>
+            <div className="about-technology"> <h2><span>Reasoning:</span></h2><p>As a boot camp graduate from General Assembly DC Chapter (Sep. 2019) and a mentor to other boot camp students, I wanted to support them during this hackathon by creating my game. Also, I enjoy building apps. </p></div>
+            <div className="about-technology"> <h2><span>Future Plan:</span></h2><p>To have autocompletion on searching characters and moving images so they load correctly. </p>
+</div>
+           </div>
            
-           <p><span>Reasoning: </span>As a boot camp graduate from General Assembly DC Chapter (Sep. 2019) and a mentor to other boot camp students, I wanted to support them during this hackathon by creating my game. Also, I enjoy building apps. </p>
            
-           <p><span>Future Plan: </span>To have autocompletion on searching characters and moving images so they load correctly. </p>
-
+           
+           
+           
                 </div>
             </div>
         )
